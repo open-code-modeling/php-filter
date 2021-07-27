aa6fb8436d315c800d27239e7a43b6dfa4c45e96
Sandro Keil <github@sandro-keil.de>
2021-07-27 07:51:15 +0200

    Switch from PHPStan to Psalm - Close #17

--- tests/FilterFactoryTest.php
+++ tests/FilterFactoryTest.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @see       https://github.com/open-code-modeling/php-filter for the canonical source repository
+ * @copyright https://github.com/open-code-modeling/php-filter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/open-code-modeling/php-filter/blob/master/LICENSE.md MIT License
+ */
+
 declare(strict_types=1);
 
 namespace OpenCodeModelingTest\Filter;
